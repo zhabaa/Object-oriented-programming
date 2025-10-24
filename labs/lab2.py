64974824c00633c3a4582873d819b371b257b8c5
zhabaa <vova.dovnar2006@gmail.com>
2025-10-24 10:36:42 +0200

    done

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import os
 
 from enum import Enum
 from types import TracebackType
@@ -32,37 +33,45 @@ class Printer:
 
     @staticmethod
     def _load_font(font_file: str) -> dict[str, list[str]]:
-        if font_file.endswith(".json"):
-            with open(font_file, "r", encoding="utf-8") as f:
-                data: dict[str, list[str]] = json.load(f)
-
-        elif font_file.endswith(".txt"):
-            data = dict()
-
-            with open(font_file, "r", encoding="utf-8") as f:
-                lines: list[str] = [line.rstrip("\n") for line in f]
-
-            current_char = str()
-            buffer: list[str] = list()
-
-            for line in lines:
-                if len(line) == 1 and line.isalpha():
-                    if current_char:
-                        data[current_char] = buffer
-
-                    current_char = line
-                    buffer = []
-
-                else:
-                    buffer.append(line)
-
-            if current_char:
-                data[current_char] = buffer
+        try:
+            if not os.path.exists(font_file): raise FileNotFoundError
+            if not os.path.isfile(font_file): raise ValueError
             
-        else: 
-            raise Exception("this file type nots implemented")
+            if font_file.endswith(".json"):
+                with open(font_file, "r", encoding="utf-8") as f:
+                    data: dict[str, list[str]] = json.load(f)
 
-        return data
+            elif font_file.endswith(".txt"):
+                data = dict()
+
+                with open(font_file, "r", encoding="utf-8") as f:
+                    lines: list[str] = [line.rstrip("\n") for line in f]
+
+                current_char = str()
+                buffer: list[str] = list()
+
+                for line in lines:
+                    if len(line) == 1 and line.isalpha():
+                        if current_char:
+                            data[current_char] = buffer
+
+                        current_char = line
+                        buffer = []
+
+                    else:
+                        buffer.append(line)
+
+                if current_char:
+                    data[current_char] = buffer
+
+            else: 
+                raise ValueError("this filetype is not implemented")
+
+            return data
+            
+        except Exception as e:
+            raise Exception(f"Error while loading {font_file}:\n{e}")
+            
 
     @classmethod
     def print_static(cls,
@@ -110,10 +119,10 @@ class Printer:
 
             sys.stdout.write(self.color.value + line + ANSI.RESET.value + "\n")
 
-# Julia
+
 with Printer(Color.RED, (1, 1), "0", "fonts/font5.json") as printer:
     printer.print("swaga")
 
 printer = Printer(Color.RED, (1, 1), "0", "fonts/font5.json")
-printer.print_static("nikita", (1, 1), Color.BLUE, 'O', "fonts/font5.json")
+printer.print_static("nikita", (10, 1), Color.BLUE, 'O', "fonts/font9.json")
 printer.print_static("good boy", (1, 1), Color.GREEN, '+', "fonts/font5.json")
